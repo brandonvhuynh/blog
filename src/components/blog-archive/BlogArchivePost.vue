bf023790412b21ac4894edc5206a40d426622aa5
Brandon Huynh <brandonvhuynh@gmail.com>
2021-10-19 11:36:11 -0600

    [blog] Added BlogArchivePost component and updated Home styling for blog archive grid layout.

---
+++ src/components/blog-archive/BlogArchivePost.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="blog-archive-post border p-3 text-center">
+    <router-link
+      v-if="post"
+      :to="{
+        name: 'Post',
+        params: {
+          id: post.id,
+          userId: post.userId,
+        },
+      }"
+      class="text-decoration-none"
+    >
+      {{ post.title }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogArchivePost",
+  props: {
+    post: {
+      default: () => {},
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,25 @@
 <template>
-  <ul v-if="posts.length" class="row justify-content-between p-0">
+  <ul v-if="posts.length" class="row justify-content-between p-0 list-unstyled">
     <li
       v-for="post in posts"
       :key="post.id"
       class="col col-12 col-lg-3 my-2 mx-1"
     >
-      <router-link
-        v-if="post"
-        :to="{
-          name: 'Post',
-          params: {
-            id: post.id,
-            userId: post.userId
-          }
-        }"
-        class="text-decoration-none"
-      >
-        {{ post.title }}
-      </router-link>
+      <BlogArchivePost :post="post" />
     </li>
   </ul>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity';
-import  fetchBlogPosts from '../utilities/fetchBlogPosts'
+import  fetchBlogPosts from '../utilities/fetchBlogPosts';
+import BlogArchivePost from '../components/blog-archive/BlogArchivePost.vue';
 
 export default {
   name: 'Home',
+  components: {
+    BlogArchivePost
+  },
   setup() {
     /**
      * Stateful Variables
